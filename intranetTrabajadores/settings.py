4dbc16bb3c971cb9111fdeea3aee0088c86b46a1
MartinLopezDev <98781432+martinskerr@users.noreply.github.com>
2023-11-16 11:45:49 -0300

    cliente agregado

--- intranetTrabajadores/settings.py
+++ intranetTrabajadores/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'codeheaders',
+    'corsheaders',
     'rest_framework',
     'trabajadorApp'
 ]
