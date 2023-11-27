41f13c73c10ebb57a0a6e955b36a9cd2a13ccc50
MartinLopezDev <98781432+martinskerr@users.noreply.github.com>
2023-11-27 15:31:04 -0300

    add superUser model and allowed host

--- trabajadorApp/models.py
+++ trabajadorApp/models.py
@@ -1,6 +1,42 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+
+class AppUserManager(BaseUserManager):
+    def create_user(self, email, password = None):
+        if not email:
+            raise ValueError('el email es requerido')
+        if not password:
+            raise ValueError('la contraseña es requerida')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password = None):
+        if not email:
+            raise ValueError('el email es requerido')
+        if not password:
+            raise ValueError('la contraseña es requerida')        
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.save()
+        return user
+    
+class appUser(AbstractBaseUser, PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, unique = True)
+    country = models.CharField(max_length=100)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+    objects = AppUserManager()
+    def __str__(self):
+        return self.username
+
 
 class Empleador(models.Model):
     nombre_empleador = models.CharField(max_length=55, null=False, blank=False)
