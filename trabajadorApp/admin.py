53c8702711393302d4096e888ecd5e9f71796f44
FelipeCatalan12 <felipeign767@gmail.com>
2023-11-27 22:10:57 -0300

    Add models to admin view django

--- trabajadorApp/admin.py
+++ trabajadorApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.apps import apps
 # Register your models here.
+
+models_app = apps.get_app_config('trabajadorApp').get_models()
+
+for model in models_app:
+    admin.site.register(model)
