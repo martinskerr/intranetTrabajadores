ac87124f44c3835875be6c0fb35454a3751a1c76
Issidora <issidora.lopez@inacapmail.cl>
2023-12-05 23:32:25 -0300

    push cambios issidora

--- clienteFront/cliente/src/pages/admin.jsx
+++ clienteFront/cliente/src/pages/admin.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Sidebar from '../components/sideBarUser';
 import MainWidget from '../components/mainWidget';
 
 
 const adminPage = () => {
     (
         <div className="flex h-screen bg-gray-200">
-            <Sidebar />
             <MainWidget />
         </div>
     )
